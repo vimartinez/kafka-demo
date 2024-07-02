c3d84d9ca813587a73bd30a004a27ea88619de34
Victor M <martinezvictor@hotmail.com>
2024-07-01 23:29:16 -0300

    add producer

---
+++ producer/src/main/java/com/kafka/demo/producer/controller/ProducerController.java
@@ -0,0 +1,23 @@
+package com.kafka.demo.producer.controller;
+
+import com.kafka.demo.producer.service.ProducerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer")
+public class ProducerController {
+
+    private final ProducerService producerService;
+
+    public ProducerController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping
+    public String publishStringMessage(@RequestBody String msg) {
+        return this.producerService.publishStringMessage(msg);
+    }
+}
